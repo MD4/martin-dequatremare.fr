f14f53450653e8fd864e8b8bfbf2d9e69c38104d
md4 <m.dequa@gmail.com>
2016-02-23 14:03:12 +0100

    Added: Google Analytics

--- src/bin/main.js
+++ src/bin/main.js
@@ -1,15 +1,34 @@
 (function main() {
     var imgs = document.querySelectorAll('img');
-    var wc = new Wc(imgs.length, start);
+    var wc = new Wc(imgs.length, function() {
+        setTimeout(start, 500);
+    });
 
     [].forEach.call(imgs, function (img) {
         img.onload = wc.trigger();
     });
 
     function start() {
+        hideMainLoader();
+        initGA();
+    }
+
+    function hideMainLoader() {
         var container = document.querySelector('#container');
         if (container) {
             container.classList.remove('hidden');
         }
     }
+
+    function initGA() {
+        if (isProduction()) {
+            ga('create', 'UA-55779062-1', 'auto');
+            ga('send', 'pageview');
+        }
+    }
+
+    function isProduction() {
+        return window.location.host === "martin-dequatremare.fr";
+    }
+
 })();
